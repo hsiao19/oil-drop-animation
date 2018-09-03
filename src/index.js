59509f2c20eaf30887cc1dc9eae2d7a7f91da6e6
yu-tung, hsiao <yutunghsiao19@gmail.com>
2018-09-03 15:13:52 +0800

    fix: render canvas in custom element sketch

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import P5 from 'p5';
 const sketchGenerator = s => {
 	const sketch = s;
 	sketch.setup = () => {
-		sketch.createCanvas(720, 400);
-		sketch.background(0);
+		sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+		sketch.background(120);
 	};
 
 	sketch.draw = () => {};
+
+	sketch.windowResized = () => {
+		sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
+		sketch.background(120);
+	};
 };
 
 // eslint-disable-next-line no-new
-new P5(sketchGenerator);
+new P5(sketchGenerator, window.document.getElementById('sketch'));
